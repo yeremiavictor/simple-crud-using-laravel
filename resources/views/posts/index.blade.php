724ca1e5856b9dc0a1572735e7fd1a1bf3b24091
yeremia Victor <yeremiavictor@gmail.com>
2023-02-06 13:58:03 +0700

    create list

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Post</title>
+    {{-- Extensi --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card shadow rounded">
+                    <div class="card-body">
+                        <a href="{{route('posts.create')}}" class="btn btn-md btn-success mb3">Baru</a>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Nama</th>
+                                    <th>Gambar</th>
+                                    <th>Keterangan</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($posts as $post)
+                                    <tr>
+                                        <td class="text-center">{{$post->nama}}</td>
+                                        <td>
+                                            <img src="{{Storage :: url('public/posts/').$post->image}}" class="rounded" style="width: 100 px" >
+                                        </td>
+                                        <td>{!! $post->keterangan !!}</td>
+                                        <td class="text-center">
+                                            <form onsubmit="return config('Anda Yakin?');" action="{{route('posts.destroy', $post->id)}}" method="POST">
+                                                <a href="{{route('posts.edit', $post->id)}}" class="btn btn-warning">Edit</a>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Hapus</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <div class="alert alert-warning">Data Masih Kosong</div>
+                                @endforelse
+                            </tbody>
+                        </table>
+                        {{$posts->links()}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- ext --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script>
+        
+        @if(session()->has('success'))
+            toastr.success('{{ session('success') }}', 'BERHASIL!'); 
+        @elseif(session()->has('error'))
+            toastr.error('{{ session('error') }}', 'GAGAL!'); 
+        @endif
+    </script>
+</body>
+</html>
