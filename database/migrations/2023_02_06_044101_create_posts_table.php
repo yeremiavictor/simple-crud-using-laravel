16cfc83ab07b2f587eada751aa670d77e742bbe3
yeremia Victor <yeremiavictor@gmail.com>
2023-02-06 11:43:25 +0700

    init, php artisan migrate

--- database/migrations/2023_02_06_044101_create_posts_table.php
+++ database/migrations/2023_02_06_044101_create_posts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('nama');
+            $table->string('gambar');
+            $table->text('keterangan');
             $table->timestamps();
         });
     }
