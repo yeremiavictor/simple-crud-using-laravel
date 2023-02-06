3fadb81c903f93db18929b5782af24d8d9b5ffbe
yeremia Victor <yeremiavictor@gmail.com>
2023-02-06 13:57:35 +0700

    create autorouting

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Memberikan route resource
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
