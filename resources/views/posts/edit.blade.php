26affa89743397de341a5ba2464b0daf3e9c18f4
yeremia Victor <yeremiavictor@gmail.com>
2023-02-06 15:22:38 +0700

    add edit views

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Post</title>
+    {{-- Extensi --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card shadow rounded">
+                    <div class="card-body">
+                        <form action="{{route('posts.update', $post->id)}}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="form-group">
+                                <label class="font-weight-bold">Nama</label>
+                                <input type="text" class="form-control @error('nama') is-invalid @enderror" name="nama" value="{{ old('nama', $post->nama)}}" placeholder="Nama">
+                                
+                                {{-- pesan --}}
+                                @error('nama')
+                                    <div class="alert alert-danger mt-2">
+                                        {{$message}}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label class="font-weight-bold">Gambar</label>
+                                <input type="file" class="form-control @error('gambar') is-invalid @enderror" name="gambar" >
+                                
+                                {{-- pesan --}}
+                                @error('gambar')
+                                    <div class="alert alert-danger mt-2">
+                                        {{$message}}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label class="font-weight-bold">Keterangan</label>
+                                <textarea type="text" class="form-control @error('keterangan') is-invalid @enderror" name="keterangan">{{ old('keterangan', $post->keterangan)}}</textarea>
+                                
+                                {{-- pesan --}}
+                                @error('keterangan')
+                                    <div class="alert alert-danger mt-2">
+                                        {{$message}}
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <button type="submit" class="btn btn-success">Simpan</button>
+
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- ext --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script>
+        CKEDITOR.replace('content');
+    </script>
+</body>
+</html>
