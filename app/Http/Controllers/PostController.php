6b1631b3aae986a365853c2016361eaa019d7492
yeremia Victor <yeremiavictor@gmail.com>
2023-02-06 15:22:26 +0700

    add function edit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,9 +5,13 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
+// Fungsi edit gambar
+use Illuminate\Support\Facades\Storage;
+
+
 class PostController extends Controller
 {
-    
+    // Detail data
     public function index()
     {
         //memperoleh data
@@ -16,32 +20,70 @@ class PostController extends Controller
         return view('posts.index', compact('posts'));
     }
 
-    public function create(){
-        return view('posts.create');
-    }
+    // Insert Data
+        public function create(){
+            return view('posts.create');
+        }
 
-    
-    public function store(Request $request){
-        //validasi
-        $this-> validate($request, [
-            'gambar'        => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
-            'nama'          => 'required|min:5',
-            'keterangan'    => 'required|min:10'
-        ]);
+        public function store(Request $request){
+            //validasi
+            $this-> validate($request, [
+                'gambar'        => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'nama'          => 'required|min:5',
+                'keterangan'    => 'required|min:10'
+            ]);
 
-        //upload gambar
-        $gambar = $request->file('gambar');
-        $gambar->storeAs('public/posts', $gambar->hashName());
+            //upload gambar
+            $gambar = $request->file('gambar');
+            $gambar->storeAs('public/posts', $gambar->hashName());
 
-        //Insert to DB
-        Post::create([
-            'nama'=> $request->nama,
-            'gambar'=> $gambar->hashname(),
-            'keterangan' => $request->keterangan
-        ]);
+            //Insert to DB
+            Post::create([
+                'nama'=> $request->nama,
+                'gambar'=> $gambar->hashname(),
+                'keterangan' => $request->keterangan
+            ]);
 
-        // Redirect halaman
-        return redirect()->route('posts.index')->with(['success' => "Tersimpan"]);
+            // Redirect halaman
+            return redirect()->route('posts.index')->with(['success' => "Tersimpan"]);
+        }
 
-    }
+    //Edit Data
+        public function edit(Post $post){
+            return view('posts.edit', compact('post'));
+        }
+        Public function update(Request $request, Post $post){
+            //validasi
+            $this-> validate($request, [
+                'gambar'        => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+                'nama'          => 'required|min:5',
+                'keterangan'    => 'required|min:10'
+            ]);
+
+            //cek apakah gambar di upload
+            if($request->hasFile('gambar')){
+                //upload gambar baru
+                $gambar = $request->file('gambar');
+                $gambar ->storeAs('public/posts', $gambar->hashName());
+                //hapus gambar lama
+                Storage::delete('public/posts/'.$post->gambar);
+                
+                //memperbarui post
+                $post->update([
+                    'nama'=> $request->nama,
+                    'gambar'=> $gambar->hashname(),
+                    'keterangan' => $request->keterangan
+                ]);
+            } else {
+                //update post saja
+                $post->update([
+                    'nama'=> $request->nama,
+                    'keterangan' => $request->keterangan
+                ]);
+            }
+
+            // Redirect halaman
+            return redirect()->route('posts.index')->with(['success' => "Tersimpan"]);
+        }
+
 }
