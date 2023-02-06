3af423333d0f5c105a68aa6ad708daef5aea1b32
yeremia Victor <yeremiavictor@gmail.com>
2023-02-06 12:23:19 +0700

    init post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        //memperoleh data
+        $posts = post::Latest()->Paginate(10);
+
+        return view('posts.index', compact('posts'));
+    }
 }
