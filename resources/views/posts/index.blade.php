fbd4da77bf48cd7b8c81655d01fe707d34d2801f
Yeremia Victor <yeremiavictor@gmail.com>
2023-02-08 10:59:26 +0700

    modified:   resources/views/posts/index.blade.php

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -34,7 +34,7 @@
                                         </td>
                                         <td>{!! $post->keterangan !!}</td>
                                         <td class="text-center">
-                                            <form onsubmit="return config('Anda Yakin?');" action="{{route('posts.destroy', $post->id)}}" method="POST">
+                                            <form onsubmit="return confirm('Anda Yakin?');" action="{{route('posts.destroy', $post->id)}}" method="POST">
                                                 <a href="{{route('posts.edit', $post->id)}}" class="btn btn-warning">Edit</a>
                                                 @csrf
                                                 @method('DELETE')
