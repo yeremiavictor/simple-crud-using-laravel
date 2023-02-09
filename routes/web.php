dfcfaeca207edb14db97f4e0d8891e3f82dcbee4
Yeremia Victor <yeremiavictor@gmail.com>
2023-02-09 12:15:33 +0700

    add lravel auth

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 
 //Memberikan route resource
 Route::resource('/posts', \App\Http\Controllers\PostController::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
